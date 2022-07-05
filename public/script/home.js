aa7e802c669973b4b25cea2c10baa48950900700
ihyamars07 <ihyamars@students.amikom.ac.id>
2022-07-05 14:04:21 +0700

    fix home page

--- public/script/home.js
+++ public/script/home.js
@@ -1,4 +1,43 @@
+async function createContentSlide() {
+  let allbox = document.getElementById('box-article')
+  let dataNews = await fetch('data/data.json').then((data) => {
+    return data.json()
+  })
+
+  console.log(dataNews)
+
+  for (let i = 0; i < 6; i++) {
+    let card = document.createElement('div')
+    card.classList.add('mySlides')
+    card.innerHTML = `
+    <div class="numbertext">${dataNews[i].id} / 6</div>
+    <img src="${dataNews[i].image}" style="width:100%">   
+      `
+    allbox.appendChild(card)
+  }
+}
+
+async function createContent() {
+  let allbox = document.getElementById('row')
+  let dataNews = await fetch('data/data.json').then((data) => {
+    return data.json()
+  })
+
+  console.log(dataNews)
+
+  for (let i = 0; i < 6; i++) {
+    let card = document.createElement('div')
+    card.classList.add('column')
+    card.innerHTML = `
+    <img class="demo cursor" src="${dataNews[i].image}" style="width:100%" onclick="currentSlide(${dataNews[i].id})" alt="${dataNews[i].title}">
+      `
+    allbox.appendChild(card)
+  }
+}
+
+createContent()
 createContentSlide()
+
 let slideIndex = 1
 showSlides(slideIndex)
 
@@ -13,7 +52,8 @@ function currentSlide(n) {
 function showSlides(n) {
   let i
   let slides = document.getElementsByClassName('mySlides')
-  let dots = document.getElementsByClassName('dot')
+  let dots = document.getElementsByClassName('demo')
+  let captionText = document.getElementById('caption')
   if (n > slides.length) {
     slideIndex = 1
   }
@@ -28,26 +68,5 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = 'block'
   dots[slideIndex - 1].className += ' active'
+  captionText.innerHTML = dots[slideIndex - 1].alt
 }
-
-async function createContentSlide() {
-    let allbox = document.getElementById('slideshow-container')
-    let dataNews = await fetch('data/data.json').then((data) => {
-      return data.json()
-    })
-
-    console.log(dataNews)
-  
-    for (let i = 0; i < 4; i++) {
-      let card = document.createElement('div')
-      card.classList.add('mySlides')
-      card.classList.add('fade')
-      card.innerHTML = `
-      <div class="numbertext">${dataNews[i].id} / 4</div>
-      <img src="${dataNews[i].image}" style="width:100%">
-      <div class="text">${dataNews[i].title}</div>    
-      `
-      allbox.appendChild(card)
-    }
-  }
-  
