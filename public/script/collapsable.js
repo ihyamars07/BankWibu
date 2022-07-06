f1c5d7aa82b117e6f9a99459bb3a06eeaf07ab0f
ihyamars07 <ihyamars@students.amikom.ac.id>
2022-07-06 11:25:57 +0700

    repair side top anime page

--- public/script/collapsable.js
+++ public/script/collapsable.js
@@ -1,6 +1,9 @@
 function toggleNavLeft() {
   let left = document.getElementById('left')
   let mainLeft = document.getElementById('mainleft')
+  let button = document.getElementById('button-left')
   left.style.width = left.style.width === '20%' ? '0' : '20%'
   mainLeft.style.marginLeft = mainLeft.style.marginLeft === '20%' ? '0' : '20%'
+  button.innerHTML =
+    button.innerHTML === 'chevron_right' ? ' chevron_left' : ' chevron_right'
 }
